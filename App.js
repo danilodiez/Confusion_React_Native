e8d6d7386361fe0bd6a3e1cea26c1447abd049e6
danilodiez <danilodiez1@hotmail.com>
2020-09-07 11:04:53 -0300

    Componente lista

--- App.js
+++ App.js
@@ -1,25 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import Main from './components/MainComponent';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>POR FIN LA CONCHA DE LA LORA</Text>
-      <StatusBar style="light" />
-      
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    color:'white'
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+    );
   }
-});
+}
